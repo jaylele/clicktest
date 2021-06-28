053223b60144dfc7fcbabce7b2571e3f7e66a9b3
许永杰 <xuyongjie@xuyongjiedeMacBook-Pro.local>
2021-06-28 18:59:35 +0800

    xx

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:jaylele/jaylele.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jaylele/jaylele.git master:gh-pages
+git push -f git@github.com:jaylele/clicktest.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath:'/clicktest'
+    publicPath:'/clicktest/'
   }
